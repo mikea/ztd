9c552077817a1380f7c1c98cb03f03eb49c1eb08
Mike Aizatsky <mike.aizatsky@gmail.com>
2022-11-19 15:30:28 -0800

    extracted geom.zig

---
+++ src/geom.zig
@@ -0,0 +1,116 @@
+const std = @import("std");
+
+pub const Vec2 = struct {
+    x: f32,
+    y: f32,
+
+    pub fn add(a: Vec2, b: Vec2) Vec2 {
+        return .{ .x = a.x + b.x, .y = a.y + b.y };
+    }
+
+    pub fn minus(a: Vec2, b: Vec2) Vec2 {
+        return .{ .x = a.x - b.x, .y = a.y - b.y };
+    }
+
+    pub fn dist(from: Vec2, to: Vec2) f32 {
+        return minus(to, from).norm();
+    }
+
+    fn dir(from: Vec2, to: Vec2) Vec2 {
+        return minus(to, from).normalized();
+    }
+
+    pub fn mul(self: *const Vec2, a: f32) Vec2 {
+        return .{ .x = self.x * a, .y = self.y * a };
+    }
+
+    pub fn norm(self: *const Vec2) f32 {
+        return @sqrt(self.x * self.x + self.y * self.y);
+    }
+
+    fn normalized(self: *const Vec2) Vec2 {
+        const n = self.norm();
+        return .{ .x = self.x / n, .y = self.y / n };
+    }
+
+    pub fn format(
+        self: Vec2,
+        comptime fmt: []const u8,
+        options: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        _ = fmt;
+        _ = options;
+
+        try writer.print("({},{})", .{
+            self.x,
+            self.y,
+        });
+    }
+
+    pub fn angle(self: *const Vec2) f32 {
+        return std.math.atan2(f32, self.y, self.x);
+    }
+};
+
+pub const Rect = struct {
+    a: Vec2,
+    b: Vec2,
+
+    pub fn centered(c: Vec2, aSize: Vec2) Rect {
+        const s2 = aSize.mul(1.0 / 2.0);
+        return .{ .a = c.minus(s2), .b = c.add(s2) };
+    }
+
+    pub fn sized(o: Vec2, aSize: Vec2) Rect {
+        return .{ .a = o, .b = o.add(aSize) };
+    }
+
+    pub fn intersects(self: *const Rect, other: Rect) bool {
+        // ((X,Y),(A,B)) and ((X1,Y1),(A1,B1))
+        // (X,Y) = (self.a.x, self.a.y)
+        // (A,B) = (self.b.x, self.b.y)
+        // (X1,Y1) = (other.a.x, other.a.y)
+        // (A1,B1) = (other.b.x, other.b.y)
+        // A<X1 or A1<X or B<Y1 or B1<Y
+        if ((self.b.x < other.a.x) or (other.b.x < self.a.x) or (self.b.y < other.a.y) or (other.b.y < self.a.y)) {
+            return false;
+        }
+        return true;
+    }
+
+    pub fn size(self: *const Rect) Vec2 {
+        return self.b.minus(self.a);
+    }
+
+    pub fn center(self: *const Rect) Vec2 {
+        return self.a.add(self.size().mul(1.0 / 2.0));
+    }
+
+    pub fn translate(self: *const Rect, v: Vec2) Rect {
+        return .{ .a = self.a.add(v), .b = self.b.add(v) };
+    }
+
+    pub fn height(self: *const Rect) f32 {
+        return self.b.y - self.a.y;
+    }
+
+    fn contains(self: *const Rect, v: Vec2) bool {
+        return v.x >= self.a.x and v.x <= self.b.x and v.y >= self.a.y and v.y <= self.b.y;
+    }
+
+    pub fn format(
+        self: Rect,
+        comptime fmt: []const u8,
+        options: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        _ = fmt;
+        _ = options;
+
+        try writer.print("[{},{}]", .{
+            self.a,
+            self.b,
+        });
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -5,11 +5,14 @@ const sdl = @cImport({
     @cInclude("SDL2/SDL_ttf.h");
 });
 const table = @import("table.zig");
+const geom = @import("geom.zig");
 const buildOptions = @import("build_options");
 
 const Table = table.Table;
 const Id = table.Id;
-const maxId = table.maxId;
+
+const Vec2 = geom.Vec2;
+const Rect = geom.Rect;
 
 const contentDir = buildOptions.content_dir;
 const SparseSet = @import("sparse_set.zig").SparseSet;
@@ -31,122 +34,6 @@ fn checkNotNull(comptime T: type, ptr: ?*T) !*T {
     return ptr orelse AppError.SdlInitError;
 }
 
-const Vec2 = struct {
-    x: f32,
-    y: f32,
-
-    fn add(a: Vec2, b: Vec2) Vec2 {
-        return .{ .x = a.x + b.x, .y = a.y + b.y };
-    }
-
-    fn minus(a: Vec2, b: Vec2) Vec2 {
-        return .{ .x = a.x - b.x, .y = a.y - b.y };
-    }
-
-    fn dist(from: Vec2, to: Vec2) f32 {
-        return minus(to, from).norm();
-    }
-
-    fn dir(from: Vec2, to: Vec2) Vec2 {
-        return minus(to, from).normalized();
-    }
-
-    fn mul(self: *const Vec2, a: f32) Vec2 {
-        return .{ .x = self.x * a, .y = self.y * a };
-    }
-
-    fn norm(self: *const Vec2) f32 {
-        return @sqrt(self.x * self.x + self.y * self.y);
-    }
-
-    fn normalized(self: *const Vec2) Vec2 {
-        const n = self.norm();
-        return .{ .x = self.x / n, .y = self.y / n };
-    }
-
-    pub fn format(
-        self: Vec2,
-        comptime fmt: []const u8,
-        options: std.fmt.FormatOptions,
-        writer: anytype,
-    ) !void {
-        _ = fmt;
-        _ = options;
-
-        try writer.print("({},{})", .{
-            self.x,
-            self.y,
-        });
-    }
-
-    pub fn angle(self: *const Vec2) f32 {
-        return std.math.atan2(f32, self.y, self.x);
-    }
-};
-
-const Rect = struct {
-    a: Vec2,
-    b: Vec2,
-
-    fn centered(c: Vec2, aSize: Vec2) Rect {
-        const s2 = aSize.mul(1.0 / 2.0);
-        return .{ .a = c.minus(s2), .b = c.add(s2) };
-    }
-
-    fn sized(o: Vec2, aSize: Vec2) Rect {
-        return .{ .a = o, .b = o.add(aSize) };
-    }
-
-    fn intersects(self: *const Rect, other: Rect) bool {
-        // ((X,Y),(A,B)) and ((X1,Y1),(A1,B1))
-        // (X,Y) = (self.a.x, self.a.y)
-        // (A,B) = (self.b.x, self.b.y)
-        // (X1,Y1) = (other.a.x, other.a.y)
-        // (A1,B1) = (other.b.x, other.b.y)
-        // A<X1 or A1<X or B<Y1 or B1<Y
-        if ((self.b.x < other.a.x) or (other.b.x < self.a.x) or (self.b.y < other.a.y) or (other.b.y < self.a.y)) {
-            return false;
-        }
-        return true;
-    }
-
-    fn size(self: *const Rect) Vec2 {
-        return self.b.minus(self.a);
-    }
-
-    fn center(self: *const Rect) Vec2 {
-        return self.a.add(self.size().mul(1.0 / 2.0));
-    }
-
-    fn translate(self: *const Rect, v: Vec2) Rect {
-        return .{ .a = self.a.add(v), .b = self.b.add(v) };
-    }
-
-    fn height(self: *const Rect) f32 {
-        return self.b.y - self.a.y;
-    }
-
-    fn contains(self: *const Rect, v: Vec2) bool {
-        return v.x >= self.a.x and v.x <= self.b.x and v.y >= self.a.y and v.y <= self.b.y;
-    }
-
-    pub fn format(
-        self: Rect,
-        comptime fmt: []const u8,
-        options: std.fmt.FormatOptions,
-        writer: anytype,
-    ) !void {
-        _ = fmt;
-        _ = options;
-
-        try writer.print("[{},{}]", .{
-            self.a,
-            self.b,
-        });
-    }
-};
-
-// -- table and things
 
 const SpriteSheet = struct {
     texture: *sdl.SDL_Texture,
@@ -463,7 +350,7 @@ const Game = struct {
         {
             // move projectiles
             const dt = 0.001 * @intToFloat(f32, ticks - self.lastTicks);
-            var toDelete = try SparseSet(Id, maxId, void).init(frameAllocator);
+            var toDelete = try SparseSet(Id, table.maxId, void).init(frameAllocator);
             defer toDelete.deinit();
 
             var it = self.projectiles.iterator();
@@ -483,10 +370,11 @@ const Game = struct {
                 if (n < ds) {
                     try toDelete.add(entry.value.target, {});
                     try toDelete.add(entry.id, {});
-                } else {
-                    const dn = dir.mul(ds / n);
-                    projectile.pos = projectile.pos.add(dn);
-                }
+                    continue;
+                } 
+
+                const dn = dir.mul(ds / n);
+                projectile.pos = projectile.pos.add(dn);
 
                 (try self.sprites.get(entry.id)).angle = dir.angle() * 360 / (2.0 * std.math.pi) - 90;
             }
